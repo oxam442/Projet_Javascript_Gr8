b68a29c3ea5c3f73f61f3366a0fa1dd72aae115f
Auka72 <adrien.chambriervitre@gmail.com>
2026-03-25 16:14:04 +0100

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,10 @@
+// popup test
+document.getElementById("texte").addEventListener("copy", function() {
+
+    window.open(
+        "popup.html",
+        "popup",
+        "width=400,height=300"
+    );
+
+});
