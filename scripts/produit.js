0409736b867f35c015a4957bf2d8fe496413713c
Auka72 <270322600+Auka72@users.noreply.github.com>
2026-04-16 20:20:57 +0200

    update

--- scripts/produit.js
+++ scripts/produit.js
@@ -1,32 +1,21 @@
 
-function changeImg(){
-    let img= document.getElementById("img1");
-    let src = img.src; 
-    if (src.includes("pin1.png")){
-        img.src="../images/pin2.png";
-    }
-    else if (src.includes("pin2.png")){
-        img.src="../images/pin3.png";
-    }
-    else if (src.includes("pin3.png")){
-        img.src="../images/pin4.png";
-    }
-    else{
-        img.src="../images/pin1.png";
-    }
+function changeImg(image){
+    let id=image.id;
+    let nb=image.dataset.nb;
+    let cpt = Math.floor(Math.random()*nb)+1;
+    image.src = `../images/${id}${cpt}.png`;
 }
 
 function achat(){
     let largeur = 600;
     let hauteur = 400;
-
     let left = (window.screen.width - largeur) / 2;
     let top = (window.screen.height - hauteur) / 2;
-
     window.open(
         "acheter.html",
         "popup",
         `width=${largeur},height=${hauteur},left=${left},top=${top}`
     );
 }
+
  
