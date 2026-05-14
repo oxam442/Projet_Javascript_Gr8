44ba79de355dff353c472649d1e77b90fbacc753
Auka72 <270322600+Auka72@users.noreply.github.com>
2026-05-14 19:39:35 +0200

    fonction js fini produit et contact

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,62 +1,79 @@
-function azerty(){
-    let thon = document.getElementById("nomContact");
-    let bar=thon.value.trim().split(" ");
-    if (bar.length>=2 && bar[0]!="" && bar[1]!=""){
+// Fonction pour verifier le nom prénom et nom
+function nomVerif(){
+    // reucperation id 
+    let id = document.getElementById("nomContact");
+    // on split pour avoir un tableau
+    let value=id.value.trim().split(" ");
+    // test pour savoir si l'entrée est vérifiée
+    if (value.length>=2 && value[0]!="" && value[1]!=""){
         document.getElementById("nomRule").style.display="none";
-        thon.style.border="2px solid green";
+        id.style.border="2px solid green";
         return true;
     }
-    else if(bar[0]!=""){
-        document.getElementById("nomRule").style.display="flex";
-        thon.style.border="2px solid red";
-        return false;
-    }
-        else{
+    // cas où il n'y a rien d'entrée
+    else if(value[0]==""){
         document.getElementById("nomRule").style.display="none";
         return false;
     }
-}
-
-function qwerty(){
-    let thon = document.getElementById("mailContact");
-    let bar=thon.value.trim().split("@");
-    if (thon.value.includes("@") && bar[1].includes(".")){   
-        document.getElementById("mailRule").style.display="none";
-        thon.style.border="2px solid green";
-        return true;
-    }
-    else if(bar[0]==""){
-        document.getElementById("mailRule").style.display="none";
-        return false;
-    }
+    // cas où il y a une erreur
     else{
-
-        document.getElementById("mailRule").style.display="flex";
-        thon.style.border="2px solid red";
+        id.style.border="2px solid red";
+        document.getElementById("nomRule").style.display="flex";
         return false;
     }
 }
 
-function zqsd(){
-    let thon = document.getElementById("messageContact");
-    if (thon.value.length>19 && thon.value.length<1000){  
-        document.getElementById("messageRule").style.display="none";
-        thon.style.border="2px solid green";
+// Fonction pour verifier l'adresse mail
+function mailVerif(){
+    // reucperation id
+    let id = document.getElementById("mailContact");
+    // on split pour avoir un tableau
+    let value=id.value.trim().split("@");
+    // test pour savoir si l'entrée est vérifiée
+    if (id.value.includes("@") && value[1].includes(".")){   
+        document.getElementById("mailRule").style.display="none";
+        id.style.border="2px solid green";
         return true;
     }
-    else if(thon.value==""){
+    // cas où il n'y a rien d'entrée
+    else if (value[0]==""){
+        document.getElementById("mailRule").style.display="none";
+        return false;
+    }
+    // cas où il y a une erreur
+    else{
+        document.getElementById("mailRule").style.display="flex";
+        id.style.border="2px solid red";
+        return false;
+    }
+}
+
+// Fonction pour verifier le message à envoyer
+function messageVerif(){
+    // reucperation id
+    let id = document.getElementById("messageContact");
+    // test pour savoir si l'entrée est vérifiée
+    if (id.value.length>19 && id.value.length<1000){  
+        document.getElementById("messageRule").style.display="none";
+        id.style.border="2px solid green";
+        return true;
+    }
+    // cas où il n'y a rien d'entrée
+    else if(id.value==""){
         document.getElementById("messageRule").style.display="none";
         return false;
     }
+    // cas où il y a une erreur
     else {
         document.getElementById("messageRule").style.display="flex";
-        thon.style.border="2px solid red";
+        id.style.border="2px solid red";
         return false;
     }
 }
 
-function verif(){
-    if (zqsd()==true && azerty()==true && qwerty()==true){ 
+// Fonction pour verifier les 3 conditions et débloquer le bouton
+function verifTotal(){
+    if (nomVerif()==true && mailVerif()==true && messageVerif()==true){ 
         document.getElementById("submitButton").style.background="var(--primary-color)";  
         document.getElementById("submitButton").disabled=false;
     }
@@ -66,44 +83,64 @@ function verif(){
     } 
 } 
 
+
 // Jeu
+// compteur
 let cpt=0;
+// Fonction pour ajouter un click au compteur
 function clickJeu(){
     cpt++;
 }
+
+// Jeu
 function jeu(event){
+    // rester sur la meme page
     event.preventDefault();
-    let thon=document.getElementById("jeu").style.display="flex";
-    let bar=document.getElementById("jeuButton");
+    // recuperation id
+    let button=document.getElementById("jeuButton");
+    let id = document.getElementById("popupJeu");
+    id.style.display="flex";
+    // définition variable
     cpt=0;
     let timeLeft=10;
-    bar.disabled=false;
-    bar.addEventListener("click", clickJeu);
-    // partie affichage à ajouter 
+    let cps=0;
+    // ajout de la fonction pour cliquer et débloque le bouton
+    button.disabled=false;
+    button.addEventListener("click", clickJeu);
+    // affichage du temps et du nombre de click
+    id.children[0].children[0].textContent=`Il reste ${timeLeft}s à jouer`;
+    id.children[0].children[1].textContent=`nombre de click : ${cpt}`;
+    // boucle du jeu
     let time = setInterval(() => {
+        // enlève le temps
         timeLeft--;
-        // partie affichage à ajouter
+        // affichage du temps et du nombre de click
+        id.children[0].children[0].textContent=`Il reste ${timeLeft}s à jouer`;
+        id.children[0].children[1].textContent=`nombre de click : ${cpt}`;
         if (timeLeft<=0){
-            bar.disabled=true; 
-            bar.removeEventListener("click",clickJeu);
+            // enlève la fonction pour cliquer et bloque le bouton
+            button.disabled=true; 
+            button.removeEventListener("click",clickJeu);
             clearInterval(time);
-            let cps= cpt/10;
-            // partie affichage à ajouter
+            cps= cpt/10;
+            // affiche le nombre de click par seconde dans la console 
+            console.log(cps);
+            // afficher le message gagné si la condition est vérifié soit un cps de 8
             if(cps>8){
-                document.getElementById("jeu").style.display="none";
-                document.getElementById("win").style.display="flex";
+                document.getElementById("popupJeu").style.display="none";
+                document.getElementById("popupWin").style.display="flex";
                 setTimeout(()=>{
                     window.location.href="../html/contact.html";
-                },3000);
-                
+                },3000);      
             }
+            // afficher le message perdu si la condition n'est pas vérifié
             else{
-                document.getElementById("jeu").style.display="none";
-                document.getElementById("loose").style.display="flex";
+                document.getElementById("popupJeu").style.display="none";
+                document.getElementById("popupLoose").style.display="flex";
                 setTimeout(()=>{
                     window.location.href="../html/contact.html";
                 },3000);
-            }
+            }   
         }
     }, 1000);
 }
--- scripts/produit.js
+++ scripts/produit.js
@@ -1,17 +1,3 @@
-// base de donnees
-const product =[
-    {nom:"service1", categorie:"services", prix:"1", description:"1", nbimg:"2"},
-    {nom:"service2", categorie:"services", prix:"1", description:"1", nbimg:"2"},
-    {nom:"service3", categorie:"services", prix:"1", description:"1", nbimg:"2"},
-    {nom:"pin", categorie:"goodies", prix:"5", nbimg:"4"},
-    {nom:"stylo", categorie:"goodies", prix:"2", nbimg:"2"},
-    {nom:"portecle", categorie:"goodies", prix:"3", nbimg:"2"},
-    {nom:"mug", categorie:"goodies", prix:"8", nbimg:"2"},
-    {nom:"tapis", categorie:"goodies", prix:"1000", nbimg:"2"},
-    {nom:"trousse", categorie:"goodies", prix:"20", nbimg:"2"},
-    {nom:"gourde", categorie:"goodies", prix:"15", nbimg:"2"},
-]
-
 // fonction pour changer aléatoirement d'image 
 function changeImg(image){
     // récupération id
@@ -24,9 +10,11 @@ function changeImg(image){
     image.src = `../images/${id}${cpt}.png`;
 }
 
-
+// Fonction pour afficher le popup pour acheter
 function popupAchat(){
+    // récupération id
     let popup=document.getElementById("popupAchat");
+    // contenu à afficher 
     let content=`
             <div class="popup">
             <div id="buttonclose" onclick="modalClose()">X</div>
@@ -34,16 +22,18 @@ function popupAchat(){
              <button onclick="popupSmiley()">🛒 ACHETER</button>
         </div>`;
 
-    // affiche le popup de confirmation 
+    // affichage du popup achat
     popup.innerHTML=content;
     popup.style.display="flex";
-}
+} 
  
+// Fonction pour afficher la confirmation de l'achat
 function popupSmiley(){
     // cache le popup d'achat
     document.getElementById("popupAchat").style.display="none";
+    // récupération id 
     let popup=document.getElementById("popupSmiley");
-    // affiche le popup du smiley 
+    // affichage du popup pour la confirmation de l'achat 
     popup.style.display="flex";
     setTimeout(()=>{
         popup.style.display="none";
@@ -52,7 +42,22 @@ function popupSmiley(){
 
 
 // filter
+// base de donnees
+const product =[
+    {nom:"Atelier Team bulding", categorie:"services", prix:"900", description:"Séance interactive pour renforcer la cohésion d'équipe. Jeux collaboratifs, ateliers de résolution de problèmes, et débriefing stratégique dans un environnement exterieur.", nbimg:"2"},
+    {nom:"Co-Walking Networking", categorie:"services", prix:"900", description:"Organisation de marches en groupe (etudiants, entrepreneurs...) pour étoffer son réseau.", nbimg:"2"},
+    {nom:"pin", categorie:"goodies", prix:"5", nbimg:"4", description:" "},
+    {nom:"stylo", categorie:"goodies", prix:"2", nbimg:"2", description:" "},
+    {nom:"portecle", categorie:"goodies", prix:"3", nbimg:"2", description:" "},
+    {nom:"mug", categorie:"goodies", prix:"8", nbimg:"2", description:" "},
+    {nom:"tapis", categorie:"goodies", prix:"1000", nbimg:"2", description:" "},
+    {nom:"trousse", categorie:"goodies", prix:"20", nbimg:"2", description:" "},
+    {nom:"gourde", categorie:"goodies", prix:"15", nbimg:"2", description:" "},
+]
+
+// Fonction pour comparer le prix
 function comparePrice(min, max, price){
+    // Tous les tests possibles sont testés
     if ((Number(price)<=Number(max) && number(price))>=Number(min)||(Number(price)<=Number(max) && min=='')||(max=='' && Number(price)>=Number(min))||(min=='' && max=='')){
         return true;
     }
@@ -61,7 +66,9 @@ function comparePrice(min, max, price){
     }
 }
 
+// Fonction pour comparer le nom du produit
 function compareWord(word, product){
+    // Tous les tests possibles sobt testés
     if (word.toLowerCase()==product ||word==''){
         return true;
     }
@@ -70,7 +77,9 @@ function compareWord(word, product){
     } 
 }
 
+// Fonction pour comparer la catégorie
 function compareCategorie(categorie, productCategorie){
+    // Tous les cas possibles sont testés
     if (categorie==productCategorie || categorie==''){
         return true;
     }
@@ -78,28 +87,40 @@ function compareCategorie(categorie, productCategorie){
         return false;
     }
 }
+
+// Fonction pour afficher les itemframe
 function afficherItemFrame(number,id){
+    // récupération id
     let itemframe = document.getElementById(id);
+    // contenu à afficher
     let content=`
         <h3>${id.toUpperCase()}</h3>
         <img src="../images/${id}1.png" id="${id}" data-nb="${product[number].nbimg}" onclick="changeImg(this)"></img>
-        <div id="prix">${product[number].prix}€</div>
+        <div> ${product[number].description}</div>
+        <div id="prix">${product[number].prix}€ à l'unité</div>
         <button id="button" onclick="popupAchat()" >🛒 ACHETER</button>`;
+    // affichage du contenu
     itemframe.innerHTML=content;
     itemframe.style.display="flex";
 
 }
+
+// Fonction pour afficher le message d'erreur lorsque les filtres ne fonctionnent pas
 function messageError(){
+    // récupération id
     let popup=document.getElementById("popupError");
-    // message d'erreur à modifier
+    // contenu à afficher
     let content=`       
     <div class="popup">
         <div id="buttonclose" onclick="modalClose()">X</div>
             <p>pas de produit trouvé</p>
     </div>`;
+    // affichage du contenu
     popup.innerHTML=content;
     popup.style.display="flex";
 }
+
+// Fonction pour appliquer tout les filtres
 function filter(){
     let cpt=0;
     for (let i=0;i<product.length;i++){
@@ -108,6 +129,7 @@ function filter(){
             element.style.display = "none";
         }   
         document.getElementById("popupError").style.display="none";
+        // utilisation de toutes les fonctions pour savoir quelle itemframe, on affiche
         if (comparePrice(document.getElementById("minPrix").value, document.getElementById("maxPrix").value, product[i].prix)==true && 
         compareCategorie(document.getElementById("categorieFilter").value,product[i].categorie)==true && 
         compareWord(document.getElementById("nomFilter").value,product[i].nom)==true){
@@ -116,6 +138,7 @@ function filter(){
         }
 
     }
+    // affichage du message d'erreur s'il n'y a les filtres ne fonctionnent pas
     if (cpt==0){
         messageError();
     }
